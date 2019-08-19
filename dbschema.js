56666187d7ef89675f37648eac8b114e64ecc368
Kevin Heard <kevin.heard@travelctm.com>
2019-08-19 11:13:10 +1000

    express and formatting data

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2019-08-18T22:58:30.628Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -3,20 +3,22 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require('express');
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello world!");
-});
-
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('screams').get()
+app.get('/screams', (req, res) => {
+  admin.firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
     .then(data => {
       let screams = [];
 
       data.forEach(doc => {
-        screams.push(doc.data())
+        screams.push({
+          screamId: doc.id,
+          ...doc.data()
+        })
       });
 
       return res.json(screams);
@@ -24,13 +26,11 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     .catch(err => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if (req.method !== 'POST') return res.status(400).json({ error: 'Method not allowed' });
-
+app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: (new Date()).toISOString()
   };
 
   admin.firestore().collection('screams').add(newScream)
@@ -42,3 +42,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+exports.api = functions.region('asia-east2').https.onRequest(app);
