97ca27f85149671a96d0f78a666f3db97178a588
Kevin Heard <kevin.heard@travelctm.com>
2019-08-20 08:59:26 +1000

    refactor and organising

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyANSJFu3hfpb6FYU0Rfy0SX4LPHZne1e_Q",
+  authDomain: "socialape-bdca8.firebaseapp.com",
+  databaseURL: "https://socialape-bdca8.firebaseio.com",
+  projectId: "socialape-bdca8",
+  storageBucket: "socialape-bdca8.appspot.com",
+  messagingSenderId: "935506996938",
+  appId: "1:935506996938:web:6fa5c90203fb4ef5"
+};
