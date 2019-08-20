6ae3346f315b2d6eecd2532d59ab0bce1813cbd6
Kevin Heard <kev@kevinheard.com>
2019-08-20 22:17:21 +1000

    getting and commenting on scream

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,28 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream
+} = require('./handlers/screams');
+
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require('./handlers/users');
 
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+// TODO: delete scream
+// TODO: like a scream
+// TODO: unlike a scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
 
 app.post('/signup', signup);
 app.post('/login', login);
